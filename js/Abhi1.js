53f5f98f316ccfe85cc15c59ff91ca3b30f878ea
goabhishek <mabhishek4156@gmail.com>
2022-11-01 18:10:13 +0530

    add Abhi.js Page

---
+++ js/Abhi1.js
@@ -0,0 +1,9 @@
+console.log('object');
+// variables
+// Ver,let,const
+
+var name = 'abhi';
+var channel;
+var marks = 34;
+channel = 'code';
+console.log(name, channel, marks);
---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.time('your');
+console.log('Hello console');
+console.log(4 + 34);
+console.log(50);
+console.log([30, 40, 50, 60, 70]);
+console.log(true);
+console.log({ hello: 'jffj', abhi: 'ansgu' });
+console.table({ hello: 'jffj', abhi: 'ansgu' });
+console.timeEnd('your');
